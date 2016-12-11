94ea884a4a21b06d2b27bdbfec312463250ef5a4
Yaroslav Leshchuk <leshukyaroslav@gmail.com>
2016-12-12 01:52:24 +0200

    Initial commit

---
+++ app/src/main/java/devinc/pomodoro_timer/MainActivity.java
@@ -0,0 +1,13 @@
+package devinc.pomodoro_timer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
